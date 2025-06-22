c587d38b1daa0fc4a5001d4d156a01a4a37abde0
kjq <1208230746@qq.com>
2025-06-22 11:41:45 +0800

    初始化项目

---
+++ src/test/java/com/kjq/langchain4j/LangChain4J.java
@@ -0,0 +1,18 @@
+package com.kjq.langchain4j;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.junit.Test;
+
+public class LangChain4J {
+
+    @Test
+    public void test01() {
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .apiKey(apiKey)
+                .modelName("gpt-4o-mini")
+                .build();
+
+        String answer = model.chat("Say 'Hello World'");
+        System.out.println(answer); // Hello World
+    }
+}
