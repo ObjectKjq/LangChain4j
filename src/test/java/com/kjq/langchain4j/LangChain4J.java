fad4b3ffad9b1860b3b1d5401f19450869bc0293
kjq <1208230746@qq.com>
2025-06-22 11:42:29 +0800

    初始化项目

--- src/test/java/com/kjq/langchain4j/LangChain4J.java
+++ src/test/java/com/kjq/langchain4j/LangChain4J.java
@@ -1,5 +1,6 @@
 package com.kjq.langchain4j;
 
+import com.kjq.langchain4j.config.ApiKeyConfig;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.junit.Test;
 
@@ -8,7 +9,7 @@ public class LangChain4J {
     @Test
     public void test01() {
         OpenAiChatModel model = OpenAiChatModel.builder()
-                .apiKey(apiKey)
+                .apiKey(ApiKeyConfig.apiKey)
                 .modelName("gpt-4o-mini")
                 .build();
 
